6088907e4e018a9ba56687c7ba61ea627b4e896a
Bessong Junior <juniorbesong8@hotmail.com>
2023-03-01 22:19:58 +0100

    output and circle ci config added

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,18 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="bessongjunior/flask-app"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run up3\ 
+	--image=$dockerpath\ 
+	--port=80 --labels app=up4
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward up4 8000:80
